b585b11e47f525fa380ca9d97ca40bad6391c7bc
SimonServant <simon@fam-diener.de>
2019-12-22 16:22:21 +0100

    Implemented super simple books page

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -3,13 +3,15 @@ import { Routes, RouterModule } from '@angular/router';
 import { ContactListComponent } from './contact-list/contact-list.component';
 import { ContactCreateComponent } from './contact-create/contact-create.component';
 import { HomeComponent } from './home/home.component';
+import { BooksComponent } from './books/books.component';
 
 
 const routes: Routes = [
   {path:  '', pathMatch:  'full', redirectTo:  'home'},
   {path: 'home', component: HomeComponent},
   {path: 'contact-create', component: ContactCreateComponent},
-  {path: 'contact-list', component: ContactListComponent}
+  {path: 'contact-list', component: ContactListComponent},
+  {path: 'books', component: BooksComponent}
 ];
 
 @NgModule({
