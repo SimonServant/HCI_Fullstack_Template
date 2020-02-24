f8dba040147a6f5ef71c7f935f266a593436c383
Shehada <yusefshehada@outlook.de>
2020-02-24 20:36:36 +0100

    Regactoring

--- Backend/Prototype/HCI_Template/serializers.py
+++ Backend/Prototype/HCI_Template/serializers.py
@@ -1,12 +1,27 @@
 from django.contrib.auth.models import User, Group
 from HCI_Template.models import Book
 from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
 
 
-class UserSerializer(serializers.HyperlinkedModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(
+        required=True,
+        validators=[UniqueValidator(queryset=User.objects.all())]
+    )
+    username = serializers.CharField(
+        validators=[UniqueValidator(queryset=User.objects.all())]
+    )
+    password = serializers.CharField(min_length=8)
+
+    def create(self, validated_data):
+        user = User.objects.create_user(validated_data['username'], validated_data['email'],
+                                        validated_data['password'])
+        return user
+
     class Meta:
         model = User
-        fields = ['url', 'username', 'email', 'groups']
+        fields = ['id', 'username', 'email', 'password']
 
 
 class BookSerializer(serializers.HyperlinkedModelSerializer):
--- Backend/Prototype/HCI_Template/views.py
+++ Backend/Prototype/HCI_Template/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from HCI_Template.models import Book
 from django.contrib.auth.models import User, Group
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 from HCI_Template.serializers import BookSerializer, UserSerializer
 
 
@@ -15,14 +15,16 @@ def index(request):
     return HttpResponse("Hello, world. You're at the HCI-prototype index.")
 
 
-# ViewSets define the view behavior.
+class GroupViewSet(viewsets.ModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = UserSerializer
+
+
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
 
-# ViewSets define the view behavior.
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-
